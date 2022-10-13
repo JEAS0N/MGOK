43dcc62c896bac460984121ca29600a3760975aa
Jeason <danilkabobkov03@gmail.com>
2022-10-14 00:25:27 +0300

    BEM

---
+++ js/main.js
@@ -0,0 +1,32 @@
+document.write ( 
+  `<div class=\"content__card\">
+    <img class= \"content__card_image\" src= \"image/info/search.svg \"> 
+    <span class=\"content__card_head\">Поиск людей</span> 
+    <span class=\"content__card_text\">Открой для себя новую ступень с <b>1 337 228</b> вакансиями от разных представителей</span> 
+  </div>
+  <div class="content__card">
+    <img class="content__card_image" src="image/info/diploma.svg">
+    <span class="content__card_head">Обучение</span>
+    <span class="content__card_text">Изучайте нужный материал на платформе в любое удобное время</span>
+  </div>
+  <div class="content__card3">
+    <img class="content__card_image" src="image/info/card.svg">
+    <span class="content__card_head">Стабильность</span>
+    <span class="content__card_text">Круглосуточный доступ к личному кабинету и всей его информации</span>
+  </div>
+  <div class="content__card">
+    <img class="content__card_image" src="image/info/leader.svg">
+    <span class="content__card_head">Умения</span>
+    <span class="content__card_text">Каждый пользователь имеет свою страничку со всеми скиллами</span>
+  </div>
+  <div class="content__card">
+    <img class="content__card_image" src="image/info/diplomat.svg">
+    <span class="content__card_head">Найм мастеров</span>
+    <span class="content__card_text">Вы сможете нанять самых лучших людей в любой сфере  деятельности человека</span>
+  </div>
+  <div class="content__card3">
+    <img class="content__card_image" src="image/info/profits.svg">
+    <span class="content__card_head">Актуальность</span>
+    <span class="content__card_text">Предоставление возможности работы в самых передовых компаниях.</span>
+  </div>`
+); 
